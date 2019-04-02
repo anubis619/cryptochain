96faf84474b9415f8353fdc67dbaa80d64eee063
Alexandru Voinescu <marius.a.voinescu@gmail.com>
2019-04-02 19:06:28 +0300

    Redone the proof of work algorithm
    integrated mining difficulty
    added Cerberus guard dog against malicious intent of modifying the block
    Integrated difficulty
    Added the option to convert Hex hash to binary

--- block.js
+++ block.js
@@ -1,5 +1,7 @@
-const { GENESIS_DATA } = require ('./config');
+const hextToBinary = require('hex-to-binary');
+const { GENESIS_DATA, MINE_RATE } = require ('./config');
 const cryptoHash = require ('./crypto-hash');
+
 
 class Block {
     constructor({timestamp, lastHash, hash, data, nonce, difficulty}){
@@ -18,16 +20,27 @@ class Block {
     static mineBlock({ lastBlock, data }){
         let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const { difficulty } = lastBlock;
+        let { difficulty } = lastBlock;
         let nonce = 0;
 
         do {
             nonce++;
             timestamp = Date.now();
+            difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp });
             hash = cryptoHash(timestamp,lastHash, data, nonce, difficulty);
-        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+        } while (hextToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({timestamp, lastHash, data, difficulty, nonce, hash});
+    }
+
+    static adjustDifficulty({originalBlock, timestamp}){
+        const { difficulty } = originalBlock;
+
+        if (difficulty <1) return 1;
+
+        if( (timestamp - originalBlock.timestamp) > MINE_RATE) return difficulty -1;
+
+        return difficulty +1
     }
 }
 
--- blockchain.js
+++ blockchain.js
@@ -37,14 +37,18 @@ class Blockchain {
 
         for(let i= 1; i<chain.length; i++){
             const {timestamp, lastHash, hash, nonce, difficulty, data} = chain[i];
-
             const actualLastHash = chain[i-1].hash;
+            const lastDifficulty = chain[i-1].difficulty;
 
             if(lastHash !== actualLastHash) return false;
 
             const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
             if(hash !== validatedHash) return false
+
+            // Cerberos guard dog - Security to check if the chain contains a tempered value
+
+            if (Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
 
         return true;
